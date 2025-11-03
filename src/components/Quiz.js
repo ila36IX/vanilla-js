3ec9cb914ae7c786cceb0ac710152af1bccac369
ila36ix <jbariali002@gmail.com>
2025-11-04 00:15:28 +0100

    better code

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,77 +1,55 @@
 import api from "../services/api.js";
 import DOMBuilder from '../services/domBuilder.js';
 
-// export default class QuizComp extends HTMLElement { 
-//   constructor () {
-//     super();
-//     this.handleQuizChange = this.handleQuizChange.bind(this);
-//     this.currentQuiz = 0;
-//     this.manyQuizes = 0;
-//   }
-//
-//   connectedCallback() {
-//     window.addEventListener('_ProjectQuizChanged', this.handleQuizChange);
-//     this.$nextQuiz = this.querySelector('#nextQuiz');
-//     this.$prevQuiz = this.querySelector('#prevQuiz');
-//     this.$progress = this.querySelector('#progress');
-//
-//     this.$nextQuiz.addEventListener('click', () => this.nextQuiz());
-//     this.$prevQuiz.addEventListener('click', () => this.prevQuiz());
-//
-//     this.render();
-//   }
-//
-//   disconnectedCallback() {
-//     window.removeEventListener('_ProjectQuizChanged', this.handleQuizChange);
-//   }
-//
-//   handleQuizChange() {
-//     if (app.data.projectQuiz) {
-//       this.quizes = app.data.projectQuiz.all;
-//       this.manyQuizes = app.data.projectQuiz.all.length;
-//       this.currentQuiz = app.data.projectQuiz.currentQuizIndex;
-//     }
-//     this.render();
-//   }
-//
-//   nextQuiz () {
-//     if (this.currentQuiz < this.manyQuizes - 1) {
-//       let $toHide = this.querySelector(`#question-${this.currentQuiz}`);
-//       let $toShow = this.querySelector(`#question-${this.currentQuiz + 1}`);
-//       if ($toHide)
-//         $toHide.classList.add('d-none');
-//       if ($toShow)
-//         $toShow.classList.remove('d-none');
-//       this.currentQuiz++;
-//       this.updateProgress();
-//     }
-//   }
-//
-//   prevQuiz () {
-//     if (this.currentQuiz > 0) {
-//       let $toHide = this.querySelector(`#question-${this.currentQuiz}`);
-//       let $toShow = this.querySelector(`#question-${this.currentQuiz - 1}`);
-//       if ($toHide)
-//         $toHide.classList.add('d-none');
-//       if ($toShow)
-//         $toShow.classList.remove('d-none');
-//       this.currentQuiz--;
-//     }
-//     this.updateProgress();
-//   }
-//
-//
-//   render() {
-//
-//   }
-// }
-
-function multipleChoices(choices) {
-  let i = 0;
-  for (let choice of choices) {
-    if (choice.is_correct) i++;
+export class QuizApp extends HTMLElement { 
+  constructor () {
+    super();
+    this.quizData = [];
+    this.currentQuestionIndex = 0;
   }
-  return i > 1;
+
+  async connectedCallback() {
+    this.innerHTML = `<p>Loading Quiz...</p>`;
+
+    try {
+      this.quizData = [
+        {
+          questionId: 1,
+          questionMarkup: '<p>What is 2 + 2?</p>',
+          choices: [
+            { id: 10, markup: '<p>3</p>' },
+            { id: 11, markup: '<p>4</p>' },
+            { id: 12, markup: '<p>5</p>' },
+          ],
+          manyCorrectAnswers: 1,
+          correctChoices: []
+        },
+        {
+          questionId: 2,
+          questionMarkup: '<p>Which are frontend frameworks?</p>',
+          choices: [
+            { id: 20, markup: '<p>React</p>' },
+            { id: 21, markup: '<p>Django</p>' },
+            { id: 22, markup: '<p>Vue</p>' },
+          ],
+          manyCorrectAnswers: 2,
+          correctChoices: []
+        }
+      ];
+      this.renderCurrentQuestion();
+
+    } catch (err) {
+      this.innerHTML = `<p>Error loading quiz...</p>`;
+      console.error(err);
+    }
+  }
+
+  renderCurrentQuestion() {
+    this.innerHTML = '';
+    const questionEl = document.createElement('quiz-question');
+    questionEl.quizData = this.quizData[this.currentQuestionIndex];
+    this.appendChild(questionEl);
+  }
 }
 
 // correctAnswersIds: null|array;
@@ -85,27 +63,31 @@ function multipleChoices(choices) {
 export default class QuizQuestion extends HTMLElement { 
   constructor () {
     super();
+    this.choices = [];
+  }
+
+  set quizData(data) {
+    this.questionId = data.questionId;
+    this.questionMarkup = data.questionMarkup;
+    this.choices = data.choices;
+    this.manyCorrectAnswers = data.manyCorrectAnswers;
+    this.correctChoices = data.correctChoices;
   }
 
   connectedCallback() {
     const template = document.getElementById("question-template");
     const content = template.content.cloneNode(true);
     this.appendChild(content);
-    this.questionMarkup = '<h1>How are you</h1>';
-    this.questionId = 2;
-    this.choices = [
-      {id: 20, markup: '<p>Fine</p>'},
-      {id: 21, markup: '<p>Good</p>'},
-      {id: 22, markup: '<p>Amazing</p>'},
-    ];
-    this.$choices = this.querySelector('#choices');
+
     this.buttonActive = true;
-    this.manyCorrectAnswers = 1;
     this.selectedChoices = [];
     this.render();
   }
 
   render() {
+    this.$choices = this.querySelector('#choices');
+    this.$question = this.querySelector('#question');
+    this.$question.innerHTML = this.questionMarkup;
     this.$choices.innerHTML = '';
     this.choices.forEach(choice => {
       const choicetemplate = `<li>
@@ -128,6 +110,7 @@ ${choice.markup}
       return ;
     this.disableButtons();
     const r = await api.answerQuestion(this.questionId, this.selectedChoices);
+    console.log(r);
     if (r.correctChoices) {
       for (let choice of r.correctChoices) {
         this.querySelector(`[data-choice_id="${choice}"]`).classList.add('border-warning');
@@ -151,6 +134,7 @@ ${choice.markup}
     this.buttonActive = true;
     for (const button of buttons) {
       button.classList.remove('border-secondary');
+      button.classList.remove('border-danger');
       button.style.cursor = 'pointer';
       button.disabled = false;
     }
@@ -158,12 +142,16 @@ ${choice.markup}
   }
 
   buttonClicked(event) {
-    this.selectedChoices.push(event.currentTarget.dataset.choice_id);
+    const clickedChoiceId = event.currentTarget.dataset.choice_id;
+    if (!this.selectedChoices.includes(clickedChoiceId)) {
+      this.selectedChoices.push(clickedChoiceId);
+      this.querySelector(`[data-choice_id="${clickedChoiceId}"]`).classList.add('border-danger');
+    }
     if (this.selectedChoices.length == this.manyCorrectAnswers)
       this.submitChoices();
   }
 }
 
 
-// customElements.define("quiz-comp", QuizComp);
 customElements.define("quiz-question", QuizQuestion);
+customElements.define("quiz-app", QuizApp);
