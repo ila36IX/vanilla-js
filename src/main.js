d18544a4efa94d27b6f9a459dccb753eaca53173
ila36IX <jbariali002@gmail.com>
2026-02-22 08:03:45 +0000

    Introduce signals

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Route from './services/Router.js';
 
 import './components/DesignSystem.js';
 
+import { createDerived, createSignal } from './services/signals.js';
+
 // you can do it also like this: window.onDOMContentLoaded = main
 window.addEventListener('DOMContentLoaded', main);
 
@@ -17,7 +19,15 @@ async function main() {
   // set up the global data state and initialise it
   loadData();
   Route.init();
+
+  var counter = createSignal(0);
+  var der = createDerived(() => counter.value * 10);
+
+  counter.subscribe((v) => console.log(v));
+
   setInterval(() => {
-    Store.time = new Date();
+    // Store.time = new Date();
+    counter.value = (v) => v + 1;
+    console.log("Derived", der.value);
   }, 2000);
 }
